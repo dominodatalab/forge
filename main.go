9f0dadac162edf3306d2623c9551ebcbb914de42
Sonny Garcia <sonnygarcia@icloud.com>
2020-01-21 16:55:45 -0700

    fixes boilerplate headerfile
    removes extra period from owners line

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Domino Data Lab, Inc..
+Copyright 2020 Domino Data Lab, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
