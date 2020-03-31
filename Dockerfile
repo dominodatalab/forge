5300733af10f2267618d7d075be1ab0778f48cb3
Sonny Garcia <sonnygarcia@icloud.com>
2020-03-31 09:21:57 -0600

    adds containerized support for builds
    updates Dockerfile to build out required components
    ports over unshare and reexec bits from img project

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,47 @@
-# Build the manager binary
-FROM golang:1.13 as builder
+FROM golang:1.14-alpine3.11 as base
 
+FROM base AS builder
+RUN apk add --no-cache build-base linux-headers
 WORKDIR /workspace
-# Copy the Go Modules manifests
-COPY go.mod go.mod
-COPY go.sum go.sum
-# cache deps before building and copying source so that we don't need to re-download as much
-# and so that source changes don't invalidate our downloaded layer
+COPY go.mod go.sum ./
 RUN go mod download
-
-# Copy the go source
 COPY main.go main.go
 COPY api/ api/
 COPY controllers/ controllers/
+COPY internal/ internal/
 COPY pkg/ pkg/
+RUN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o forge \
+    && cp forge /usr/bin/
 
-# Build
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o manager main.go
+FROM base AS runc
+RUN apk add --no-cache bash build-base git libseccomp-dev linux-headers
+WORKDIR /go/src/github.com/opencontainers/runc
+RUN git clone https://github.com/opencontainers/runc.git . \
+    && git checkout 7cb3cde1f49eae53fb8fff5012c0750a64eb928b
+RUN make static BUILDTAGS="seccomp apparmor" \
+    && cp runc /usr/bin/
 
-# Use distroless as minimal base image to package the manager binary
-# Refer to https://github.com/GoogleContainerTools/distroless for more details
-FROM gcr.io/distroless/static:nonroot
-WORKDIR /
-COPY --from=builder /workspace/manager .
-USER nonroot:nonroot
+FROM base AS idmap
+RUN apk add --no-cache autoconf automake build-base byacc gettext gettext-dev git libcap-dev libtool libxslt
+WORKDIR /shadow
+RUN git clone https://github.com/shadow-maint/shadow.git . \
+    && git checkout 59c2dabb264ef7b3137f5edb52c0b31d5af0cf76
+RUN ./autogen.sh --disable-nls --disable-man --without-audit --without-selinux --without-acl --without-attr --without-tcb --without-nscd \
+    && make \
+    && cp src/newuidmap src/newgidmap /usr/bin/
 
-ENTRYPOINT ["/manager"]
+FROM alpine:3.11
+COPY --from=builder /usr/bin/forge /usr/bin/forge
+COPY --from=runc /usr/bin/runc /usr/bin/runc
+COPY --from=idmap /usr/bin/newuidmap /usr/bin/newuidmap
+COPY --from=idmap /usr/bin/newgidmap /usr/bin/newgidmap
+RUN chmod u+s /usr/bin/newuidmap /usr/bin/newgidmap \
+  && adduser -D -u 1000 user \
+  && mkdir -p /run/user/1000 \
+  && chown -R user /run/user/1000 /home/user \
+  && echo user:100000:65536 | tee /etc/subuid | tee /etc/subgid
+USER user
+ENV USER user
+ENV HOME /home/user
+ENV XDG_RUNTIME_DIR=/run/user/1000
+CMD ["/usr/bin/forge"]
--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"flag"
 	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
 
+	"github.com/opencontainers/runc/libcontainer/system"
+	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -12,6 +17,7 @@ import (
 
 	forgev1alpha1 "github.com/dominodatalab/forge/api/v1alpha1"
 	"github.com/dominodatalab/forge/controllers"
+	_ "github.com/dominodatalab/forge/internal/unshare"
 	"github.com/dominodatalab/forge/pkg/container"
 	// +kubebuilder:scaffold:imports
 )
@@ -35,6 +41,8 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.Parse()
+
+	reexec()
 
 	ctrl.SetLogger(zap.New(func(o *zap.Options) {
 		o.Development = true
@@ -70,5 +78,72 @@ func main() {
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "Problem running manager")
 		os.Exit(1)
+	}
+}
+
+func reexec() {
+	if len(os.Getenv("IMG_DO_UNSHARE")) <= 0 && system.GetParentNSeuid() != 0 {
+		setupLog.Info("Preparing to unshare process namespace")
+
+		var (
+			pgid int
+			err  error
+		)
+
+		// On ^C, or SIGTERM handle exit.
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			for sig := range c {
+				logrus.Infof("Received %s, exiting.", sig.String())
+				if err := syscall.Kill(-pgid, syscall.SIGKILL); err != nil {
+					logrus.Fatalf("syscall.Kill %d error: %v", pgid, err)
+					continue
+				}
+				os.Exit(0)
+			}
+		}()
+
+		// If newuidmap is not present re-exec will fail
+		if _, err := exec.LookPath("newuidmap"); err != nil {
+			logrus.Fatalf("newuidmap not found (install uidmap package?): %v", err)
+		}
+
+		// Initialize and re-exec with our unshare.
+		cmd := exec.Command("/proc/self/exe", os.Args[1:]...)
+		cmd.Env = append(os.Environ(), "IMG_DO_UNSHARE=1")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
+		if err := cmd.Start(); err != nil {
+			logrus.Fatalf("cmd.Start error: %v", err)
+		}
+
+		pgid, err = syscall.Getpgid(cmd.Process.Pid)
+		if err != nil {
+			logrus.Fatalf("getpgid error: %v", err)
+		}
+
+		var (
+			ws       syscall.WaitStatus
+			exitCode int
+		)
+		for {
+			// Store the exitCode before calling wait so we get the real one.
+			exitCode = ws.ExitStatus()
+			_, err := syscall.Wait4(-pgid, &ws, syscall.WNOHANG, nil)
+			if err != nil {
+				if err.Error() == "no child processes" {
+					// We exited. We need to pass the correct error code from
+					// the child.
+					os.Exit(exitCode)
+				}
+
+				logrus.Fatalf("wait4 error: %v", err)
+			}
+		}
 	}
 }
