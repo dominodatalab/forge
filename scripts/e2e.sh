f874569e7edd7ab96f62806ab570a47b2bebb192
Sonny Garcia <sonnygarcia@icloud.com>
2020-05-14 14:45:30 -0600

    fixes helm issue
    rabbitmq chart version was wrong
    adds required param to e2e script
    passes newly minted docker image url to script

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -12,6 +12,12 @@ function info {
   echo -e "--> \033[1;32m$1\033[0m"
 }
 
+if [[ -z $1 ]]; then
+  echo -e "Run integration tests against a Forge OCI image.\n\nUsage: $0 image"
+  exit 1
+fi
+
+image="$1"
 namespace="forge-test-$(tr -cd 'a-z0-9' < /dev/urandom | fold -w10 | head -n1)"
 
 info "Creating test namespace: $namespace"
@@ -28,9 +34,17 @@ helm install docker-registry stable/docker-registry \
 
 info "Installing RabbitMQ chart"
 helm install rabbitmq stable/rabbitmq \
-  --version 3.8.2 \
+  --version 6.18.2 \
   --namespace "$namespace"
 
 info "Launching Forge controller"
+echo "launching $image"
+
+#kubectl run forge \
+#  --image "$image" \
+#  --namespace "$namespace" \
+#  --labels "app=forge" \
+#  --timeout 30s \
+#  --wait
 
 info "All tests ran successfully"
