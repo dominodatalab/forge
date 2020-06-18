c61f117a5a86f8ccad826dc386f59bcf0716fb65
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2020-06-17 23:52:09 -0700

    add a progress writer to track push

--- internal/builder/embedded/bkimage/push.go
+++ internal/builder/embedded/bkimage/push.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/go-logr/logr"
+	"github.com/moby/buildkit/util/progress"
 	"github.com/moby/buildkit/util/push"
 )
 
@@ -28,11 +30,32 @@ func (c *Client) PushImage(ctx context.Context, image string) error {
 	c.logger.Info(fmt.Sprintf("Pushing image %q", image))
 	defer c.logger.Info(fmt.Sprintf("Pushed image %q", image))
 
+	progressWriter := progress.NewMultiWriter()
+	progressWriter.Add(&logrProgressWriter{c.logger})
+
 	// push with context absent session to avoid authorizer override
 	// see github.com/moby/buildkit@v0.7.1/util/resolver/resolver.go:158 for more details
-	ctx = context.Background()
+	ctx = progress.WithProgress(context.Background(), progressWriter)
 
 	// "insecure" param is not used in the following call
 	insecure := false
 	return push.Push(ctx, sm, c.contentStore, imgObj.Target.Digest, image, insecure, c.getRegistryHosts(), false)
+}
+
+type logrProgressWriter struct {
+	logger logr.Logger
+}
+
+func (l *logrProgressWriter) Write(id string, value interface{}) error {
+	l.logger.Info(id, "progress", value)
+	return nil
+}
+
+func (l *logrProgressWriter) WriteRawProgress(progress *progress.Progress) error {
+	l.logger.Info(progress.ID, "progress", progress)
+	return nil
+}
+
+func (l *logrProgressWriter) Close() error {
+	return nil
 }
