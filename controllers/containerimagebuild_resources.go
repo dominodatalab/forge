ca4de5e00a6622b42baa00a082a3ccd4adb1a908
Fernando Correia <fernando@dominodatalab.com>
2021-03-02 00:24:11 +0000

    init container WIP

--- controllers/containerimagebuild_resources.go
+++ controllers/containerimagebuild_resources.go
@@ -333,6 +333,25 @@ func (r *ContainerImageBuildReconciler) createJobForBuild(ctx context.Context, c
 		volumeMounts = append(volumeMounts, *forgeBuildMount)
 	}
 
+	// include any init containers provided in the custom resource
+	for order, initContainer := range cib.Spec.InitContainers {
+		var env []corev1.EnvVar
+		for _, envVar := range initContainer.Env {
+			env = append(env, corev1.EnvVar{
+				Name:  envVar.Name,
+				Value: envVar.Value,
+			})
+		}
+		initContainers = append(initContainers, corev1.Container{
+			Name:         fmt.Sprintf("spec-init-%v", order),
+			Image:        initContainer.Image,
+			Command:      initContainer.Command,
+			Args:         initContainer.Args,
+			Env:          env,
+			VolumeMounts: []corev1.VolumeMount{buildContextDirVolumeMount},
+		})
+	}
+
 	resources := corev1.ResourceRequirements{
 		Limits:   corev1.ResourceList{},
 		Requests: corev1.ResourceList{},
