6a4e92e13268cdbea402c019d580ac5e9811bd94
Nick Jablonski <nick.jablonski@dominodatalab.com>
2021-05-11 11:32:08 -0400

    pass toleration in as array

--- controllers/containerimagebuild_resources.go
+++ controllers/containerimagebuild_resources.go
@@ -416,7 +416,8 @@ func (r *ContainerImageBuildReconciler) createJobForBuild(ctx context.Context, c
 	}
 
 	if r.JobConfig.ImagePullSecret != "" {
-		var toleration = corev1.Toleration{
+		var toleration = [1]corev1.Toleration
+    toleration[0] = corev1.Toleration{
 			Effect: "NoSchedule",
 			Key: r.JobConfig.TolerationKey,
 			Operator: "Exists",
