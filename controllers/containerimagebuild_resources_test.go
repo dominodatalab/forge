5ea0bdcd56bd5aed57f25bb7e451571b3e96d5a7
Nick Jablonski <nick.jablonski@dominodatalab.com>
2021-05-19 23:34:40 -0400

    test fixes

--- controllers/containerimagebuild_resources_test.go
+++ controllers/containerimagebuild_resources_test.go
@@ -210,10 +210,8 @@ func TestContainerImageBuildReconciler_initContainers(t *testing.T) {
 func TestContainerImageBuildReconciler_tolerations(t *testing.T) {
 	controller := makeController(t)
 	cib := &forgev1alpha1.ContainerImageBuild{}
-	r := &ContainerImageBuildReconciler{
-		JobConfig: &BuildJobConfig{TolerationKey: "toleration1"},
-	}
-	require.NoError(t, r.createJobForBuild(context.Background(), cib))
+	controller.JobConfig.TolerationKey = "toleration1"
+	require.NoError(t, controller.createJobForBuild(context.Background(), cib))
 	job := &batchv1.Job{}
 	require.NoError(t, controller.Client.Get(context.Background(), types.NamespacedName{Name: cib.Name}, job))
 	expected := corev1.Toleration{
